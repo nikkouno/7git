26ca8a0e5617e340026de878b068145279a32b6e
lan <lan@loghorizon.com>
2022-03-30 15:59:41 +0800

    first-commit

---
+++ src/main/java/com/lan/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.lan.git;
+
+/**
+ * @author lan
+ * @create 2022-03-30 15:55
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello,git!");
+    }
+}
